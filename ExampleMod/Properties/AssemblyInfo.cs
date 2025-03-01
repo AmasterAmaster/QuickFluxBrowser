4c82155df0dae89ec53a31181c9679b162303be5
Andrew Kosub <amasteramastertv@gmail.com>
2025-03-01 15:44:05 -0600

    Initial commit, testing & learning

---
+++ ExampleMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("QuickFluxBrowser")]
+[assembly: AssemblyProduct("QuickFluxBrowser")]
+[assembly: AssemblyDescription("A mod that lets you spawn out a flux browser faster.")]
+[assembly: AssemblyCompany("AmasterAmaster")]
+[assembly: AssemblyCopyright("Copyright © 2025 AmasterAmaster")]
+[assembly: AssemblyVersion(QuickFluxBrowser.QuickFluxBrowser.VERSION_CONSTANT)]
+[assembly: AssemblyFileVersion(QuickFluxBrowser.QuickFluxBrowser.VERSION_CONSTANT)]
---
+++ ExampleMod/QuickFluxBrowser.cs
@@ -0,0 +1,95 @@
+using FrooxEngine;
+using HarmonyLib;
+using ResoniteModLoader;
+using ResoniteHotReloadLib;
+using FrooxEngine.ProtoFlux;
+using System.Reflection;
+
+namespace QuickFluxBrowser;
+//More info on creating mods can be found https://github.com/resonite-modding-group/ResoniteModLoader/wiki/Creating-Mods
+public class QuickFluxBrowser : ResoniteMod
+{
+	internal const string VERSION_CONSTANT = "1.0.0"; //Changing the version here updates it in all locations needed
+	public override string Name => "QuickFluxBrowser";
+	public override string Author => "AmasterAmaster";
+	public override string Version => VERSION_CONSTANT;
+	public override string Link => "https://github.com/resonite-modding-group/ExampleMod/";
+
+	const string harmonyId = "com.AmasterAmaster.QuickFluxBrowser";
+	static ModConfiguration config;
+
+	public override void OnEngineInit()
+	{
+		HotReloader.RegisterForHotReload(this);
+
+		// Get the config if needed
+		config = GetConfiguration();
+
+		// Call setup method
+		Setup();
+	}
+
+	// This is the method that should be used to unload your mod
+	// This means removing patches, clearing memory that may be in use etc.
+	static void BeforeHotReload()
+	{
+		// Unpatch Harmony
+		Harmony harmony = new Harmony(harmonyId);
+		harmony.UnpatchAll(harmonyId);
+
+		// clear any memory that is being used
+	}
+
+	// This is called in the newly loaded assembly
+	// Load your mod here like you normally would in OnEngineInit
+	static void OnHotReload(ResoniteMod modInstance)
+	{
+		// Get the config if needed
+		config = modInstance.GetConfiguration();
+
+		// Call setup method
+		Setup();
+	}
+
+	static void Setup()
+	{
+		// Patch Harmony
+		Harmony harmony = new Harmony(harmonyId);
+		harmony.PatchAll();
+	}
+
+	/*[HarmonyPatch(typeof(ProtoFluxTool), "OpenNodeBrowser", typeof(IButton), typeof(ButtonEventData))]
+	//MethodInfo(typeof(ProtoFluxTool), "OpenNodeBrowser", typeof(IButton), typeof(ButtonEventData));
+	class QuickFluxBrowserPatch
+	{
+		// Print a message when something happens (Just for example)
+		public static void Postfix(ProtoFluxTool __instance)
+		{
+			//When "I" is pressed...
+			if(Engine.Current.InputInterface.GetKeyDown(Key.I))
+			{
+				__instance.LocalUser.
+			}
+		}
+	}*/
+
+	[HarmonyPatch(typeof(InputInterface), "Update", typeof(float))]
+	class QuickFluxBrowserPatch
+	{
+		public static MethodInfo protoflux_Browser = MethodInfo(typeof(ProtoFluxTool), "OpenNodeBrowser", typeof(IButton), typeof(ButtonEventData));
+		public static MethodInfo keys = MethodInfo(typeof(InputInterface), "get_keys");
+
+		// Print a message when something happens (Just for example)
+		public static void Postfix(InputInterface __instance)
+		{
+			
+
+			//When "I" is pressed...
+			if(Engine.Current.InputInterface.GetKeyDown(Key.I))
+			{
+				//__instance.LocalUser.
+				keys.GetValue(__instance, );
+			}
+		}
+	}
+}
